687980727ffb54f10930c929740ae352107f0702
Lossalt <107666363+Lossalt@users.noreply.github.com>
2022-06-19 14:37:22 +0800

    Add files via upload

---
+++ mobile.php
@@ -0,0 +1,8 @@
+<?php
+//随机数生成
+$num = rand(1,2);
+//图片地址拼接
+$picpath = "https://raw.githubusercontent.com/Lossalt/ramdom-img/main/mobile/".$num.".jpg";
+//重定位到图片
+die(header("Location: $picpath"));
+?>
