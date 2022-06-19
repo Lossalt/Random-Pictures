cd447934ba3f4900dc06f1ee3ce717c831351bd8
Lossalt <107666363+Lossalt@users.noreply.github.com>
2022-06-19 14:37:57 +0800

    Update mobile.php

--- mobile.php
+++ mobile.php
@@ -2,7 +2,7 @@
 //随机数生成
 $num = rand(1,2);
 //图片地址拼接
-$picpath = "https://raw.githubusercontent.com/Lossalt/ramdom-img/main/mobile/".$num.".jpg";
+$picpath = "https://raw.githubusercontent.com/Lossalt/ramdom-img/main/mobile/".$num.".webp";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
